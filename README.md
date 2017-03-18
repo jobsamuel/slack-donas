b32080826ba054a6caf10476bcb21b31eb1514d8
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2017-03-18 14:50:51 -0400

    Init

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
+<img src="https://raw.githubusercontent.com/Ranks/emojione/master/assets/png_512x512/0023-20e3.png" alt="Keycap Number Sign" width="50" height="50"/> <img src="https://raw.githubusercontent.com/Ranks/emojione/master/assets/png_512x512/1f369.png" alt="Doughnut" width="50" height="50"/>
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/simpleloops/slack-donas/tree/master)
+
 # slack-donas
-Un webhook que responde a un comando en Slack
+> Un webhook que responde a un comando en Slack
+
+## pre-requisitos
+
+- [Node](https://nodejs.org/es/) (Para las pruebas locales)
+- [VPS](https://m.do.co/c/2f6ba054b7bc) (En caso de que no quieras utilizar Heroku en Producción)
+- [Comando en Slack](https://api.slack.com/slash-commands)
+
+## uso
+
+```bash
+$ npm install
+$ npm start
+```
+
+## ig-post
+
+[Ver publicación en Instagram](https://www.instagram.com/p/BRw-WrljGcV/)
+
+## licencia
+[Licencia MIT](http://opensource.org/licenses/MIT) :copyright: 2017 simpleloops
---
+++ donas.js
@@ -0,0 +1,55 @@
+/* slack-donas
+ *
+ * Un webhook creado con Node y Express
+ * que se encarga de responder el comando
+ * "/donas" accionado en Slack.
+ *
+ * Licencia MIT © 2017 simpleloops
+ */
+
+// Dependencias
+const express = require('express');
+const server = express();
+
+/* Configuración del puerto.
+ *
+ * Permite el funcionamiento en el entorno local
+ * y en el de producción; por ejemplo, en Heroku.
+ */
+server.set('port', process.env.PORT || 3000);
+
+/* Método GET.
+ *
+ * Una vez inicializado el servidor, al acceder
+ * (al enviar un HTTP GET Request) a la URL
+ * el mismo responderá un mensaje indicando que
+ * todo está bien.
+ */
+server.get('/', (req, res) => {
+  res.send('OK');
+});
+
+/* Método POST.
+ *
+ * Le permite al API de Slack comunicarse con
+ * el servidor (enviar un HTTP POST Request).
+ * Es aquí donde se envían los emojis de las donas
+ * para que sean publicados en el canal de Slack
+ * donde fue accionado el comando.
+ */
+server.post('/', (req, res) => {
+  res.send(':doughnut: :doughnut: :coffee:');
+});
+
+/* Inicialización del servidor.
+ *
+ * Es aquí donde el servidor comienza a trabajar,
+ * es decir, donde comienza a "escuchar" peticiones
+ * (HTTP Requests) hechas al puerto previamente
+ * configurado. Si todo está bien, debería mostrarse
+ * (sólo una vez) en la consola el mensaje definido
+ * en el callback.
+ */
+server.listen(server.set('port'), () => {
+  console.log('Todo anda chévere ✔');
+});
